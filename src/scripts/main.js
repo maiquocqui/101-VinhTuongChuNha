711665707d15558172ba561a80a6dd798cd743ba
maiquocqui <maiquocqui@gmail.com>
2018-06-19 13:19:49 +0700

    Initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+    $('.product-slide').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        fade: true,
+        asNavFor: '.product-nav'
+    });
+
+    $('.product-nav').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.product-slide',
+        dots: false,
+        centerMode: false,
+        arrows: false,
+        focusOnSelect: true
+    });
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+});
